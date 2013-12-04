200d152a4a9853a3d8473d29ce306abbe88ea126
Jonathan Perry-Houts <jperryhouts@gmail.com>
2013-12-03 18:08:25 -0800

    Clarified some variable names, added a few comments.

--- refine.m
+++ refine.m
@@ -1,7 +1,7 @@
-function [p,t] = refine(p,t,tri)
-    %  p   : Nx2 array of nodal XY coordinates, [x1,y1; x2,y2; etc]
-    %  t   : Mx3 array of triangles as indices, [n11,n12,n13; n21,n22,n23; etc]
-    %  tri : Mx1 logical array, with tri(k) = TRUE if kth triangle is to be refined
+function [p,t] = refine(p,t,ref)
+    % p   : Nx2 array of nodes' XY coordinates
+    % t   : Mx3 array of elements as node indices
+    % ref : Mx1 logical array; ref(x) = 1 if xth element is to be refined
 
     DEBUG = false;
 
@@ -16,28 +16,28 @@ function [p,t] = refine(p,t,tri)
             2 3 5; ...
             2 5 7; ...
             7 5 8];
-        tri = [0; 1; 0; 0; 0; 1];
+        ref = [0; 1; 0; 0; 0; 1];
     else
         if nargin ~= 3
             error('Wrong number of inputs');
         end
-        if numel(tri) ~= size(t,1)
-            error('tri must be an Mx1 array');
+        if size(ref,1) ~= size(t,1)
+            error('ref must be an Mx1 array');
         end
     end
 
-    tri = logical(tri);
+    ref = logical(ref);
 
     %% find edges which are to be split
-    numt = size(t,1);
-    vect = 1:numt;
+    nume = size(t,1);
+    vect = 1:nume;
 
     e = [t(:,[1,2]); t(:,[2,3]); t(:,[3,1])];
-    [e,j,k] = unique(sort(e,2),'rows'); %% set of edges
-    te = [k(vect), k(vect+numt),k(vect+2*numt)]; %% Edges in each triangle
+    [e,j,k] = unique(sort(e,2),'rows'); %% Set of edges
+    te = [k(vect), k(vect+nume),k(vect+2*nume)]; %% Edges organized by element
 
-    split = false(size(e,1),1);
-    split(te(tri,:)) = true;
+    split = false(size(e,1),1); % Tracks which edges will be split.
+    split(te(ref,:)) = true;
 
     %% For triangles with only two split edges, split all 3 edges instead.
     while any(sum(double(split(te)),2) == 2)
@@ -45,11 +45,11 @@ function [p,t] = refine(p,t,tri)
         split(te(newsplits,:)) = true;
     end
 
-    %% Get set of triangles which will be split
-    split1 = sum(double(split(te)),2) == 1;
-    split3 = sum(double(split(te)),2) == 3;
+    %% Get logical arrays of elements which will be split
+    split2 = sum(double(split(te)),2) == 1; % Elements to be cut in 2
+    split4 = sum(double(split(te)),2) == 3; % Elements to be cut in 4
 
-    %% Add new nodes
+    %% Add new nodes at midpoints of split edges
     nnodes0 = size(p,1);
     newnodes = 0.5 * (p(e(split,1),:) + p(e(split,2),:));
     p = [p; newnodes];
@@ -58,11 +58,11 @@ function [p,t] = refine(p,t,tri)
     i = zeros(1,size(e,1));
     i(split) = (1:length(find(split))) + nnodes0;
 
-    tret = t(~(split1|split3),:); % temporary variable for new triangles
+    tret = t(~(split2|split4),:); % temporary variable for new elements
 
     %% Split refined triangles in to 4 triangles
-    if any(split3)
-        tnodes = [t(split3,:) i(te(split3,:))];
+    if any(split4)
+        tnodes = [t(split4,:) i(te(split4,:))];
         tret = [tret;tnodes(:,1),tnodes(:,4),tnodes(:,6)];
         tret = [tret;tnodes(:,4),tnodes(:,2),tnodes(:,5)];
         tret = [tret;tnodes(:,5),tnodes(:,3),tnodes(:,6)];
@@ -70,14 +70,14 @@ function [p,t] = refine(p,t,tri)
     end
 
     %% Split triangles with 1 hanging node in to 2 triangles
-    newnodes = sum(i(te(split1,:))');
-    othernodes = t(split1,:);
+    newnodes = sum(i(te(split2,:))');
+    originalnodes = t(split2,:);
     cnodes = e(split,:);
-    for j=1:length(find(split1)) % for each triangle being split
-        % find the node opposite the new hanging node
+    for j=1:length(find(split2)) % for each triangle being split
+        % find the node opposite the hanging node
         newnode = newnodes(j);
         connected_nodes = cnodes(newnode-nnodes0,:);
-        othernode = setdiff(othernodes(j,:), connected_nodes);
+        othernode = setdiff(originalnodes(j,:), connected_nodes);
         tret = [tret; newnode, othernode, connected_nodes(1)];
         tret = [tret; newnode, connected_nodes(2), othernode];
     end
